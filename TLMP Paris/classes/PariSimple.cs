376674eefcfe243787b77684cb94704d75d52ed9
Tsumiki <49730009+TsumikiVR@users.noreply.github.com>
2022-10-18 13:52:12 +0200

    Created structure

---
+++ TLMP Paris/classes/PariSimple.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TLMP_Paris.Classe
+{
+    internal class PariSimple : Pari
+    {
+        int eliminations;
+        public int Eliminations { get => eliminations; set => eliminations = value; }
+        public PariSimple(int elimination,DateTime dateMax,DateTime DateMatch,string libelles,int pointearn, int idparis):base(dateMax,DateMatch,libelles,pointearn,idparis)
+        {
+            this.Eliminations = elimination;
+        }
+        public void setResult(int result)
+        {
+            this.ResultMatch = result;
+        }
+    }
+}
