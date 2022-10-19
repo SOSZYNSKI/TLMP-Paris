fa8f371b83c93bfd0f65314316bca890abf112e8
Fastasix <97157791+Fastasix@users.noreply.github.com>
2022-10-19 10:52:45 +0200

    creation classe connexion a la base de donnée

---
+++ TLMP Paris/classes/DBUtils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace TLMP_Paris.classes
+{
+    public class DBUtils
+    {
+        public static SqlConnection()    
+        {
+            GetDBConnection(string datasource, string database, string username, string password);
+        }
+}
