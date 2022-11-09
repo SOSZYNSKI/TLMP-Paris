8094f432958d87a1b021b742d84d6b70ad5a2188
Fastasix <97157791+Fastasix@users.noreply.github.com>
2022-11-09 10:53:20 +0100

    ajout ado parie

---
+++ TLMP Paris/classes/ADOparie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TLMP_Paris.classes
+{
+    public class ADOparie
+    {
+        public static SqlConnection connexion = new SqlConnection("Data Source=sql.reseau-labo.fr;User ID=admin_ltmp_db;Password=ltmp_admin_553;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+        public static void Close()
+        {
+            connexion.Close();
+        }
+
+        public static void Open()
+        {
+            connexion.Open();
+        }
+    }
+}
