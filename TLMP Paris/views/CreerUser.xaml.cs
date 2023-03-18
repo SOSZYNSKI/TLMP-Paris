d3fae184e3a28f8e4304043ab3f30a5564118ad7
Tsumiki <49730009+TsumikiVR@users.noreply.github.com>
2023-03-18 16:50:47 +0100

    Edit/Del user & others

--- TLMP Paris/views/CreerUser.xaml.cs
+++ TLMP Paris/views/CreerUser.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -32,11 +33,15 @@ namespace TLMP_Paris.views
         private void btn_creer_Click(object sender, RoutedEventArgs e)
         {
             List<string> errors = new();
-            if (txt_box_nom.Text == string.Empty || txt_box_password.Text == string.Empty || txt_box_prenom.Text == string.Empty || txt_box_points.Text == string.Empty)
+            Regex stringPattern = new("^[a-zA-Z0-9]{3,60}$");
+            Regex passwordPattern = new("^(?=.*[a-z])(?=.*[A-Z]).{8,30}$");
+            Regex usernamePattern = new("^[a-zA-Z0-9]+$");
+            if (txt_box_nom.Text == string.Empty || txt_box_password.Text == string.Empty || txt_box_prenom.Text == string.Empty || txt_box_points.Text == string.Empty || txt_box_userlogin.Text == string.Empty)
             {
                 errors.Add("Une ou plusieurs entrées sont vides :");
                 if (txt_box_nom.Text == string.Empty) errors.Add("- Nom");
                 if (txt_box_prenom.Text == string.Empty) errors.Add("- Prénom");
+                if (txt_box_userlogin.Text == string.Empty) errors.Add("- Nom d'Utilisateur");
                 if (txt_box_password.Text == string.Empty) errors.Add("- Mot de passe");
                 if (txt_box_points.Text == string.Empty) errors.Add("- Total de points");
                 MessageBox.Show(string.Join('\n', errors.ToArray()), "Erreur, entrées vides", MessageBoxButton.OK);
@@ -53,8 +58,28 @@ namespace TLMP_Paris.views
                 MessageBox.Show("Le total de points n'est pas un nombre", "Erreur, entrée invalide", MessageBoxButton.OK);
                 return;
             };
+            if(stringPattern.IsMatch(txt_box_nom.Text) == false || stringPattern.IsMatch(txt_box_prenom.Text) == false || passwordPattern.IsMatch(txt_box_password.Text) == false || usernamePattern.IsMatch(txt_box_userlogin.Text) == false)
+            {
+                errors.Add("Une ou plusieurs entrées ne sont pas comformes :");
+                if (stringPattern.IsMatch(txt_box_nom.Text) == false) errors.Add("- Nom");
+                if (stringPattern.IsMatch(txt_box_prenom.Text) == false) errors.Add("- Prénom");
+                if (usernamePattern.IsMatch(txt_box_userlogin.Text) == false) errors.Add("- Nom d'Utilisateur");
+                if (passwordPattern.IsMatch(txt_box_password.Text) == false) errors.Add("- Mot de passe");
+                MessageBox.Show(string.Join('\n', errors.ToArray()), "Erreur, entrées non conformes", MessageBoxButton.OK);
+                return;
+            }
+            if(txt_box_confirmpassword.Text != txt_box_password.Text)
+            {
+                MessageBox.Show("Les mot de passes ne sont pas similaires", "Erreur, mot de passe invalide", MessageBoxButton.OK);
+                return;
+            };
             User newUser = new(txt_box_prenom.Text, txt_box_nom.Text, 0, txt_box_password.Text, txt_box_userlogin.Text, userTotalPoints, -1, MainWindow.promotions.ToList()[c_box_promotion.SelectedIndex].IdPromotion);
             MainWindow.Users.Add(newUser);
+            txt_box_nom.Text = "";
+            txt_box_password.Text = "";
+            txt_box_points.Text = "";
+            txt_box_prenom.Text = "";
+            txt_box_userlogin.Text = "";
             MessageBox.Show("Utilisateur crée avec succès !", "Utilisateur crée", MessageBoxButton.OK);
         }
     }
--- TLMP Paris/views/GestionInscription.xaml.cs
+++ TLMP Paris/views/GestionInscription.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -68,6 +69,39 @@ namespace TLMP_Paris
                 if (txtbox_searchby_lastname.Text.Length < 1 && txtbox_searchby_firstname.Text.Length < 1 && txtbox_searchby_promotion.Text.Length < 1 && cbox_searchby_firstname.SelectedIndex < 1 && cbox_searchby_lastname.SelectedIndex < 1 && cbox_searchby_promotion.SelectedIndex < 1) return true;
                 return tmpCheck;
             }).ToList();
+        }
+
+        private void btn_supprimer_user_Click(object sender, RoutedEventArgs e)
+        {
+            object user = tbl_tableau_classement.SelectedItem;
+            User selectedUser = user as User;
+            MainWindow.Users.Remove(selectedUser);
+            tbl_tableau_classement.Items.Refresh();
+        }
+
+        private void btn_modifier_user_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbl_tableau_classement.SelectedIndex == null) return;
+            object user = tbl_tableau_classement.SelectedItem;
+            List<string> errors = new();
+            Regex passwordPattern = new("^(?=.*[a-z])(?=.*[A-Z]).{8,30}$");
+            Regex usernamePattern = new("^[a-zA-Z0-9]+$");
+            if (txt_box_userlogin.Text == string.Empty || txt_box_password.Text == string.Empty || passwordPattern.IsMatch(txt_box_password.Text) == false || usernamePattern.IsMatch(txt_box_userlogin.Text) == false)
+            {
+                errors.Add("Il y a plusieurs erreurs :");
+                if (txt_box_userlogin.Text == string.Empty) errors.Add("- Le nom d'utilisateur est vide");
+                if (txt_box_password.Text == string.Empty) errors.Add("- Le mot de passe est vide");
+                if (usernamePattern.IsMatch(txt_box_userlogin.Text) == false) errors.Add("- Le nom d'utilisateur n'est pas conforme");
+                if (passwordPattern.IsMatch(txt_box_password.Text) == false) errors.Add("- Le mot de passe n'est pas conforme");
+                MessageBox.Show(string.Join('\n', errors.ToArray()), "Erreurs", MessageBoxButton.OK);
+                return;
+            }
+            User selectedUser = user as User;
+            MainWindow.Users.Where(c => c.IdUser == selectedUser.IdUser).ToList().ForEach(userF =>
+            {
+                userF.UserLogin = txt_box_userlogin.Text;
+                userF.UserPassword = txt_box_password.Text;
+            })
         }
     }
 }
